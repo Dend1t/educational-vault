4528c3a68f1f09c7db95578cd14dd851710f6c45
Dend1t <my.dendit.ar@gmail.com>
2026-03-23 00:21:21 +0300

    Training

--- Language C/Training/main.cpp
+++ Language C/Training/main.cpp
@@ -1,10 +1,16 @@
 #include<iostream>
+#include<math.h>
 
 int main(int argc, char const *argv[])
 {
-    int a, b;
-    float d;
-    scanf("%d %d %f", &a, &b, &d);
-    printf("%d %d %f \n", a, b, d);
+    float a,b;
+    scanf("%f %f", &a, &b);
+    if(pow(b,2) + pow(a,2) > pow(a+b,2) ){
+        printf("Сумма квадратов больше %f \n " , pow(a,2));
+    }
+    else{
+        a = a<0 ? a : pow(a,2);
+        printf("%f",a);
+    }
     return 0;
 }
