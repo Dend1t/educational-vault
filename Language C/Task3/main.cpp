ad370cc3a73a6c106670fc58fa7d4c8abb734592
Dend1t <my.dendit.ar@gmail.com>
2026-04-05 21:24:41 +0300

    Add char[] method to Task3

--- Language C/Task3/main.cpp
+++ Language C/Task3/main.cpp
@@ -3,18 +3,29 @@
 
 int main(int argc, char const *argv[])
 {
-    int arr[] = {10, 20, 30, 40, 50};
+    int arr_1[] = {10, 20, 30, 40, 50};
+    int *head_1 = arr_1;
 
-    int *head = arr;
-
-    for (int i = 0; i < sizeof(arr) / sizeof(arr[0]) - 1; i++)
+    for (int i = 0; i < sizeof(arr_1) / sizeof(arr_1[0]) - 1; i++)
     {
-        int temp = *(head + i);
-        arr[i] = *(head + i + 1);
-        arr[i + 1] = temp;
-        printf("%d\n", *(head + i));
+        int temp = *(head_1 + i);
+        arr_1[i] = *(head_1 + i + 1);
+        arr_1[i + 1] = temp;
+        printf("%d\n", arr_1[i]);
     }
-    printf("%d\n", *(head + sizeof(arr) / sizeof(arr[0]) - 1));
+    printf("%d\n", arr_1[sizeof(arr_1) / sizeof(arr_1[0])-1]);
+
+    char arr_2[] = "abcdef";
+    char *head_2 = arr_2;
+
+    for (int i = 0; i < sizeof(arr_2) / sizeof(arr_2[0]) - 1; i++)
+    {
+        int temp = *(head_2 + i);
+        arr_2[i] = *(head_2 + i + 1);
+        arr_2[i + 1] = temp;
+        printf("%c\n", arr_2[i]);
+    }
+    printf("%c\n", arr_2[sizeof(arr_2) / sizeof(arr_2[0])-1]);
 
     return 0;
 }
--- Language C/Training/main.cpp
+++ Language C/Training/main.cpp
@@ -9,8 +9,19 @@ int main(int argc, char const *argv[])
     // {
     //     printf(" Element %d: = %d , Adres: %p\n", i, *(p + i), (p + i));
     // }
-    
 
+    
+    int arr_1[] = {10,20,30,40,50,60};
+    int* head_int = arr_1;
+    for( int i = 0 ; i<sizeof(arr_1)/sizeof(arr_1[0]); i+=2){
+        printf("%d \n",*(head_int+i));
+    }
+
+    char arr_2[] = "abcdef";
+    char* head_char = arr_2;
+    for( int i = 0 ; i<sizeof(arr_2)/sizeof(arr_2[0]); i+=2){
+        printf("%c \n",*(head_char+i));
+    }
 
 
 
