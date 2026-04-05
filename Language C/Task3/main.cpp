996c77009ce1fec581f984439ad064cf434d5bb3
Dend1t <my.dendit.ar@gmail.com>
2026-04-05 20:47:44 +0300

    Task3

---
+++ Language C/Task3/main.cpp
@@ -0,0 +1,22 @@
+#include <iostream> 
+#include <math.h>
+
+int main(int argc, char const *argv[])
+{
+    int arr[]= { 10 , 20, 30 ,40 ,50};
+
+    int *head = arr;
+    
+    for(int i = 0; i < sizeof(arr)/sizeof(arr[0])-1; i++){
+        int temp = *(head+i);
+        arr[i] = *(head+i+1);
+        arr[i+1] = temp;
+        printf("%d\n", *(head+i));
+    }
+    printf("%d\n", *(head+sizeof(arr)/sizeof(arr[0])-1));
+
+
+
+
+    return 0;
+}
