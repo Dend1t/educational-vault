3c8d20c0fa35ce57b49d7ffcebfad55636fccaeb
Dend1t <my.dendit.ar@gmail.com>
2026-04-15 19:00:09 +0300

    Task3

--- Algorithms and Data Structure/Task3/src/Main.java
+++ Algorithms and Data Structure/Task3/src/Main.java
@@ -4,21 +4,16 @@ public class Main {
 
     public static void main(String[] args) throws Exception {
 
-        Deque<Integer> standardQueue = new ArrayDeque<Integer>();
+        Deque<Buyer> standardQueue = new ArrayDeque<Buyer>();
 
-        Deque<Integer> myQueue = new SimpleDeque<Integer>();
+        Deque<Buyer> myQueue = new SimpleDeque<Buyer>();
 
         // Теперь можем передавать в один и тот же метод и стандартные и свои
         // реализации. Метод не знает что именно мы ему передали, т.к. работает через
         // интерфейс
         TaskLogic.solve(standardQueue);
         TaskLogic.solve(myQueue);
+        
         new Buyer(2, 2, 2, 2).print();
-
-        // Теперь можем передавать в один и тот же метод и стандартные и свои
-        // реализации. Метод не знает что именно мы ему передали, т.к. работает через
-        // интерфейс
-        TaskLogic.solve(standardQueue);
-        TaskLogic.solve(myQueue);
     }
 }
--- Algorithms and Data Structure/Task3/src/TaskLogic.java
+++ Algorithms and Data Structure/Task3/src/TaskLogic.java
@@ -1,17 +1,50 @@
+import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.Deque;
+import java.util.Random;
 
 public class TaskLogic {
+    static class SortByAverageTime implements Comparator{
+        public int compare(Object o1, Object o2) {
+            Buyer b1 = (Buyer) o1;
+            Buyer b2 = (Buyer) o2;
 
-    // Метод который решает вашу задачу
-    // Демонстрация работы со стеком и очередью
-    static void solve(Deque<Integer> queue) {
-        // Добавляем элементы в очередь
-        queue.add(100);
-        queue.add(200);
-        queue.add(300);
+            if (o1 == null) {
+                return 1;
+            }
+            if (o2 == null) {
+                return -1;
+            }
+
+            if (b1.a == b2.a)
+                return 0;
+            return b1.a > b2.a ? 1 : -1;
+        }
+    }
+    
+    static public ArrayList<Buyer> buyers = new ArrayList<Buyer>();
+    static final Comparator comparator = new SortByAverageTime();
+    
+    
+    static void addBuyer(Integer id, Integer s, Integer t, Integer n){
+        buyers.addLast(new Buyer(id, s, t, n));
+    }
+
+    static void solve(Deque<Buyer> queue) {
+        // Добавляем элементы в стек
+        Random random = new Random();
+        for(int i = 0; i < 5; i++){
+            buyers.addLast(new Buyer(i, random.nextInt(10), random.nextInt(10), random.nextInt(10)));
+        }
+        buyers.sort(comparator);
+
+        for(int i = 0; i < 5; i++){
+            queue.add(buyers.get(i));
+        }
 
         // Удаляем и выводим элементы из очереди
-        System.out.println("Queue poll: " + queue.poll());
-        System.out.println("Queue poll: " + queue.poll());
+        for(int i = 0; i < 5; i++){
+            System.out.println("Queue poll: " + queue.poll());
+        }
     }
 }
