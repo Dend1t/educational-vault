fd1e94f84122ac8f0a96c331eccc93b477f5de1d
Dend1t <my.dendit.ar@gmail.com>
2026-04-20 11:15:45 +0300

    Pointers Matrix Lesson

--- Language C/PointersMatrix/main.cpp
+++ Language C/PointersMatrix/main.cpp
@@ -10,8 +10,18 @@ int** createArray(int ROW, int COL) {
             //arr[j]
         }
     }
-
     return arr;
+}
+int** createLinearArray(int ROW, int COL) {
+    int* line = (int*)malloc(ROW * sizeof(int) * COL);
+    int** arr = (int**)malloc(ROW * sizeof(int*));
+    for (int i = 0; i < ROW; i++) {
+        *(arr + i) = line + i * COL;
+
+        for (int j = 0; j < COL; j++) {
+            *(*(arr + i) + j) = i+1;
+        }
+    }
 }
 
 int main() {
